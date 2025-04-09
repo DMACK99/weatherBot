c123552bb837a8da248610baf26d8b3aab171b63
DMACK99 <86076249+DMACK99@users.noreply.github.com>
2025-04-08 21:59:31 -0600

    Add files via upload

--- weatherGrabber.py
+++ weatherGrabber.py
@@ -29,10 +29,13 @@ def get_wx():
 
 def main():
     wx = get_wx()
+    #the number of 12hr periods I would like to check
     num_pers = 7
+
     #this was a sarcastic response, we don't need a daily sarcastic message, just one when wind is bad
     nowarning = ["It is lovely weather we are having"]
     warning = []
+
     #this is where I define the high wind speeds
     highwinds = ["45", "50", "55", "60", "65", "70", "75"]
     if wx:
@@ -70,26 +73,52 @@ def main():
         size = len(info)
         for f in range(size):
 
-            #reset out to be blank
-            out = [''] * 17
-
             #splits report into individual lines, easier for formatting.
             temp = info[f].split('\n')
             tempsize = len(temp)
+            #reset out to be blank
+            out = [''] * tempsize
 
-            #for loop ensures we always have an index of 16 - some reports are shorter than others
+            #for loop ensures we always have the right sized array, some reports are different lengths
             for i in range(tempsize):
                 out[i] = temp[i]
+                #test line: print(out[i])
             
             report = ["", "", "", "", "", "", ""]
+            #find smart indexes
+            for i in out:
+                if "name" in i:
+                     nameLine = out.index(i)
+                if "startTime" in i:
+                     startTimeLine = out.index(i)
+                if "endTime" in i:
+                     endTimeLine = out.index(i)
+                if "temperature" in i:
+                     temperatureLine = out.index(i)
+                if "windSpeed" in i:
+                     windSpeedLine = out.index(i)
+                if "windDirection" in i:
+                     windDirLine = out.index(i)
+                if "detailedForecast" in i:
+                    forecastLine = out.index(i)
+
             #this is where the report array is created
-            report[0] = ('WEATHER REPORT: ' + out[1][7:]) 
-            report[1] = ('Start Time: ' +  out[2][23:31] + 'L')
-            report[2] = ('End Time: ' + out[3][21:29] + 'L')
-            report[3] = ('Temperature: ' + out[5][14:] + 'F')
-            report[4] = ('Wind Speed: ' + out[10][12:])
-            report[5] = ('Wind Direction: ' + out[11][16:])
-            report[6] = ('Detailed Forecast: ' + out[14][19:] + '' + out[15] + ' ' + out[16]  + '\n')
+            report[0] = ('WEATHER REPORT: ' + out[nameLine][7:]) 
+            report[1] = ('Start Time: ' +  out[startTimeLine][23:31] + 'L')
+            report[2] = ('End Time: ' + out[endTimeLine][21:29] + 'L')
+            report[3] = ('Temperature: ' + out[temperatureLine][14:] + 'F')
+            report[4] = ('Wind Speed: ' + out[windSpeedLine][12:])
+            report[5] = ('Wind Direction: ' + out[windDirLine][16:])
+            
+            #some reports have detailed forcasts split into multiple lines
+            extraLines = tempsize - forecastLine - 1
+            #test line: print(extraLines)
+            report[6] = ('Detailed Forecast: ' + out[forecastLine][19:])
+            i = 1
+            while i < extraLines:
+                report[6] = (report[6] + '' + out[forecastLine+i])
+                i += 1
+            report[6] = (report[6] + " \n")
 
             for i in out[10].split(' '):
                 for j in highwinds:
@@ -98,10 +127,14 @@ def main():
                         break
                         
         if len(nowarning) > len(warning):
+            #a generic message could be sent daily when there is no upcoming weather event
             print(nowarning)
             return nowarning
         else:
-            print(warning)
+            #this is my 'pretty print'
+            for i in warning:
+                for j in i:
+                    print(j)
             return warning
         
     else:
